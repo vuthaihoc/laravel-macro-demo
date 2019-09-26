5d47f54749cb80ebf517e908f5d4b336921a58d5
Vu Thai Hoc <vuthaihoc.ttud@gmail.com>
2019-09-26 23:26:18 +0700

    added solution

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Container\Container;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Arr;
 use Illuminate\Support\ServiceProvider;
+use Intervention\Image\Facades\Image;
+use Illuminate\Contracts\Filesystem\Factory as FilesystemFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +18,27 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        UploadedFile::macro( 'saveTo', function($path, $options = []){
+            
+            $height = Arr::pull( $options, 'height');
+            $width = Arr::pull( $options, 'width');
+            $aspectRatio = Arr::pull( $options, 'aspectRatio');
+            $upsize = Arr::pull( $options, 'upsize');
+            $quality = Arr::pull( $options, 'quality', 90);
+            
+            $image = Image::make( $this );
+            $image->resize( $width, $height, function ($constraint) use ($aspectRatio, $upsize) {
+                if($aspectRatio){
+                    $constraint->aspectRatio();
+                }
+                if($upsize){
+                    $constraint->upsize();
+                }
+            });
+            
+            return $image->save($path, $quality);
+            
+        });
     }
 
     /**
